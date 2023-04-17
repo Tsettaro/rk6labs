5ffedea33207e32efd4cc79d10f891d007214fcf
Artur <43388317+Tsettaro@users.noreply.github.com>
2023-04-17 13:12:44 +0300

    EDIT: main.cpp

--- OOP/Labs/5/main.cpp
+++ OOP/Labs/5/main.cpp
@@ -10,9 +10,9 @@ private:
 public:
     Point() : _x(0), _y(0) {};
     Point(double x, double y) : _x(x), _y(y) {};
-    static Point from_stdin();
     double getX() {return _x ;}
     double getY() {return _y ;}
+    static Point from_stdin();
     static double distance(Point&, Point&);
 };
 
