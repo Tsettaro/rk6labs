3b02833ec52cb10f7764d781b25ca5bdca71225f
Artur <43388317+Tsettaro@users.noreply.github.com>
2023-09-19 09:49:50 +0000

    EDIT: test.cpp

--- OOP/test.cpp
+++ OOP/test.cpp
@@ -19,13 +19,13 @@ class Figure {
 };
 
 bool Figure::deskout(char * p) {
-	return ((p[0] > 'h') || (p[0] < 'a') || (p[1] < '1') || (p[1] > '8'));
+	return ((p[0] > 'h') || (p[0] < 'a') || (p[1] < '1') || (p[1] > '8')); // && 
 }
 
 void Figure::desk() {
 	int i, j;
 	char s[2];
-	const char * mark = ".+";
+	const char mark = '.';
 	char m;
 	cout << "  a b c d e f g h\n";
 	for (i = 8; i > 0; i--) {
@@ -33,7 +33,7 @@ void Figure::desk() {
 		s[1] = '0' + i;
 		for (j=0; j < 8; j++) {
 			s[0] = 'a' + j;
-			m = (!(*this == s)) ? mark[attack(s)] : isa();
+			m = (!(*this == s)) ? mark : isa();
 			cout << m << " ";
 		}
 		cout << i << '\n';
@@ -43,22 +43,43 @@ void Figure::desk() {
 }
 
 class Rook : virtual public Figure {
+	private:
+		int cur_player;
 	public:
-		Rook(char * p) : Figure(p) {};
+		Rook(char * p) : Figure(p), cur_player(1) {};
         char isa() {return 'R';};
 		int attack(char * p);
+		void game();
 };
 
+void Rook::game(){
+	this->desk();
+	if (cur_player == 1) {
+            std::cout << "Ход игрока (Введите число полей для перестановки): ";
+            int move;
+            std::cin >> move;
+
+            if (!makeMove(move)) {
+                std::cout << "Неверный ход! Игра завершается." << std::endl;
+                return;
+            }
+        } else {
+            std::cout << "Ход компьютера: ";
+            int move = -1;
+            while (move < 0 || move > 7) {
+                move = rand() % 9 - 1;
+            }
+            makeMove(move);
+            std::cout << move << std::endl;
+        }
+
+        cur_player = (cur_player == 1) ? 2 : 1;
+}
+
 int Rook::attack(char * p) {
-    if (deskout(p))
-		return 0;
-    int x = abs(p[0] - pos[0]);
-    int y = abs(p[1] - pos[1]);
-    if ((x + y > 0 && y == 0) || (x + y > 0 && x == 0))
-        return 1;
-    return 0;
+    
 }
 int main() {
 	Rook k("h8");
-	k.desk();
+	k.game();
 }
