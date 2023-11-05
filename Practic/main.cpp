b835ed11055e4a53cd5801c75f6a8b4cb1a9e71b
Artur <atchemerzov@yandex.ru>
2023-11-05 19:40:49 +0300

    main file

--- Practic/main.cpp
+++ Practic/main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <cstdio>
-int main(){
+#include <vector>
+
+class ATM{
+	int nominal;
+	int count;
+	public:
+		ATM(int n, int c): nominal(n), count(c){}
+		int getNom(){return nominal;}
+		int getCount(){return count;}
+};
+
+void init(std::vector <ATM> &atms){
 	FILE *fp;
-	fp = fopen("example.txt", "r");
-	int nom, count;
-	while(fscanf("%d – %d", fp, &nom, &count)){
-		std::cout << nom << " " << count << std::endl;
-	}
+    fp = fopen("example.txt", "r");
+    int nom, count, a;
+    while((a = fscanf(fp, "%d - %d", &nom, &count)) != EOF){
+		if (a != 2) exit(-1);
+		atms.push_back(ATM(nom, count));
+    }
 	fclose(fp);
+}
+
+int main(){
+	std::vector <ATM> atms;
+    init(atms);
+	std::cout << atms.size() << std::endl;
 }
