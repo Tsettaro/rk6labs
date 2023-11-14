059e33c2aa738a1f61e069470604c27cc1e973c1
Artur <atchemerzov@yandex.ru>
2023-11-15 01:10:19 +0300

    FOR FIX

--- Teorver/2/main.cpp
+++ Teorver/2/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
-#include <iomanip>
-using std::cin;
+#include <vector>
+using std::cin, std::vector;
 
 int factorial(int i)
 {
@@ -10,26 +10,31 @@ int factorial(int i)
 }
 
 void first_task(long double lambda, long double nu){
-    long double sum = 0, M = 0, k = 0, ro = lambda/nu;
+    vector<long double> p;
+    long double sum = 0, M = 0, k = 0, ro = lambda / nu, p0 = 0;
     for (int i = 0; i < 4; i++){
-        long double p_0 = 1/(sum + pow(ro, i)/factorial(i));
-        sum += p_0;
-        long double p = pow(ro, i)/factorial(i) * p_0;
-        M += p * i;
-        std::cout << "n = " << i << " p = " << p << " M = " << M;
-        if (i > 0) std::cout << " k = " << M/i;
-        std::cout << '\n';
+        if (i == 0) {
+            p0 = 1;
+        } else {
+            p0 = (long double) pow(ro, i) / factorial(i);
+        }
+        std::cout << p0 << '\n';
+        p.push_back(p0);
+        sum += p0;
     }
+    p0 = 1 / sum;
 }
 
+
+
 int main(){
-    int R1, G1, B1, R2, G2, B2, R3, G3, B3;
+    /*int R1, G1, B1, R2, G2, B2, R3, G3, B3;
     cin >> R1 >> G1 >> B1;
     cin >> R2 >> G2 >> B2;
     cin >> R3 >> G3 >> B3;
 
-    long double T_c = R1 + G1 + B1, T_s = R2;
-    long double lambda = 1/T_c, nu = 1/T_s;
+    long double T_c = R1 + G1 + B1, T_s = R2;*/
+    long double lambda = 1/19, nu = 1/7;
 
     first_task(lambda, nu);
 }
